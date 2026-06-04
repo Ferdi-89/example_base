024ad2ec19bd9399694ca2d64e3f81aeed6397c6
ferdi-89 <fpaasgord@gmail.com>
2026-06-04 09:37:54 +0700

    done2

--- database/factories/matakuliahFactory.php
+++ database/factories/matakuliahFactory.php
@@ -18,8 +18,8 @@ class matakuliahFactory extends Factory
     public function definition(): array
     {
         return [
-            'kode_matakuliah' => $this->faker->unique()->regexify('[A-Z]{3}[0-9]{3}'),
-            'nama_matakuliah' => $this->faker->sentence(3),
+            'kode_matakuliah' => $this->faker->unique()->regexify('RPL-[0-9]{3}'),
+            'nama_matakuliah' => $this->faker->randomElement(['Algoritma dan Pemrograman','Struktur Data','Basis Data','Jaringan Komputer','Sistem Operasi','Kecerdasan Buatan','Machine Learning','Big Data','Keamanan Sistem Informasi','Analisis dan Desain Sistem Informasi','Rekayasa Perangkat Lunak','Pengembangan Aplikasi Web','Pengembangan Aplikasi Mobile','Testing Perangkat Lunak','Manajemen Proyek Perangkat Lunak','Etika Profesi','Kewirausahaan','Bahasa Inggris','Olahraga','Seni Budaya']),
             'semester' => $this->faker->numberBetween(1, 6),
             'jenis_matakuliah' => $this->faker->randomElement(['Teori', 'Praktek']),
             'sks' => $this->faker->numberBetween(1, 4),
